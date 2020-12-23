81bd94a3952556ea33b66c53b5979f666c444bee
DTrifonov <denislav.trifonov.1995@gmail.com>
2020-12-23 06:36:29 +0200

    Initial commit

---
+++ Debug/BlueNRG_MS/App/sensor.d
@@ -0,0 +1,191 @@
+BlueNRG_MS/App/sensor.o: ../BlueNRG_MS/App/sensor.c \
+ ../BlueNRG_MS/App/sensor.h ../BlueNRG_MS/App/gatt_db.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_def.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_types.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gap.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/link_layer.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_def.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gap_aci.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/hci_le.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/link_layer.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/hci_const.h \
+ ../Middlewares/ST/BlueNRG-MS/hci/hci_tl_patterns/Basic/hci_tl.h \
+ ../BlueNRG_MS/Target/hci_tl_interface.h \
+ ../Core/Inc/stm32l5xx_nucleo_bus.h ../Core/Inc/stm32l5xx_nucleo_conf.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal.h \
+ ../Core/Inc/stm32l5xx_hal_conf.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rcc.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_def.h \
+ ../Drivers/CMSIS/Device/ST/STM32L5xx/Include/stm32l5xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32L5xx/Include/stm32l552xx.h \
+ ../Drivers/CMSIS/Include/core_cm33.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Include/mpu_armv8.h \
+ ../Drivers/CMSIS/Device/ST/STM32L5xx/Include/system_stm32l5xx.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rcc_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_gpio.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_gpio_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_dma.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_dma_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_cortex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_adc.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_ll_adc.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_adc_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_exti.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash_ramfunc.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_i2c.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_i2c_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_icache.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pcd.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_ll_usb.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pcd_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pwr.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pwr_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rtc.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rtc_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_spi.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_spi_ex.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_uart.h \
+ ../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_uart_ex.h \
+ ../Drivers/BSP/STM32L5xx_Nucleo/stm32l5xx_nucleo_errno.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_types.h \
+ ../Middlewares/ST/BlueNRG-MS/utils/ble_list.h \
+ ../BlueNRG_MS/Target/bluenrg_conf.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/compiler.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_aci_const.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/hci_const.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gatt_server.h \
+ ../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gatt_aci.h
+
+../BlueNRG_MS/App/sensor.h:
+
+../BlueNRG_MS/App/gatt_db.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_def.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_types.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gap.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/link_layer.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_def.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gap_aci.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/hci_le.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/link_layer.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/hci_const.h:
+
+../Middlewares/ST/BlueNRG-MS/hci/hci_tl_patterns/Basic/hci_tl.h:
+
+../BlueNRG_MS/Target/hci_tl_interface.h:
+
+../Core/Inc/stm32l5xx_nucleo_bus.h:
+
+../Core/Inc/stm32l5xx_nucleo_conf.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal.h:
+
+../Core/Inc/stm32l5xx_hal_conf.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rcc.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_def.h:
+
+../Drivers/CMSIS/Device/ST/STM32L5xx/Include/stm32l5xx.h:
+
+../Drivers/CMSIS/Device/ST/STM32L5xx/Include/stm32l552xx.h:
+
+../Drivers/CMSIS/Include/core_cm33.h:
+
+../Drivers/CMSIS/Include/cmsis_version.h:
+
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+
+../Drivers/CMSIS/Include/mpu_armv8.h:
+
+../Drivers/CMSIS/Device/ST/STM32L5xx/Include/system_stm32l5xx.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rcc_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_gpio.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_gpio_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_dma.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_dma_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_cortex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_adc.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_ll_adc.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_adc_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_exti.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_flash_ramfunc.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_i2c.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_i2c_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_icache.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pcd.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_ll_usb.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pcd_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pwr.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_pwr_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rtc.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_rtc_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_spi.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_spi_ex.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_uart.h:
+
+../Drivers/STM32L5xx_HAL_Driver/Inc/stm32l5xx_hal_uart_ex.h:
+
+../Drivers/BSP/STM32L5xx_Nucleo/stm32l5xx_nucleo_errno.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_types.h:
+
+../Middlewares/ST/BlueNRG-MS/utils/ble_list.h:
+
+../BlueNRG_MS/Target/bluenrg_conf.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/compiler.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_aci_const.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/hci_const.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gatt_server.h:
+
+../Middlewares/ST/BlueNRG-MS/includes/bluenrg_gatt_aci.h:
